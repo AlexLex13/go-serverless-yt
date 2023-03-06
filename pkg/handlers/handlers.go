a04553d6798f1ef37532a72b570aa78c1163dea7
Alexey <suxovarov.alexey@gmail.com>
2023-03-06 14:46:09 +0300

    add handler GetUser

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -3,7 +3,10 @@ package handlers
 import (
 	"net/http"
 
+	"github.com/AlexLex13/go-serverless-yt/pkg/user"
 	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
 )
 
 var ErrorMethodNotAllowed = "Method is not allowed"
@@ -12,7 +15,23 @@ type ErrorBody struct {
 	ErrorMsg *string `json:"error,omitempty"`
 }
 
-func GetUser() {
+func GetUser(req events.APIGatewayProxyRequest, tableName string, dynaClient dynamodbiface.DynamoDBAPI) (
+	*events.APIGatewayProxyResponse, error) {
+
+	email := req.QueryStringParameters["email"]
+	if len(email) > 0 {
+		result, err := user.FetchUser(email, tableName, dynaClient)
+		if err != nil {
+			return apiResponse(http.StatusBadRequest, ErrorBody{aws.String(err.Error())})
+		}
+		return apiResponse(http.StatusOK, result)
+	}
+
+	result, err := user.FetchUsers(tableName, dynaClient)
+	if err != nil {
+		return apiResponse(http.StatusBadRequest, ErrorBody{aws.String(err.Error())})
+	}
+	return apiResponse(http.StatusOK, result)
 
 }
 

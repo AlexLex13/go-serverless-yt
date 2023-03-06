ee984ba0398368944b0e0c0dfc8daeab86838f3c
Alexey <suxovarov.alexey@gmail.com>
2023-03-06 12:18:26 +0300

    add func handler

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"os"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -19,4 +20,21 @@ func main() {
 	}
 	dynaClient := dynamodb.New(awsSession)
 	lambda.Start(handler)
+}
+
+const tableName = "LambdaInGoUser"
+
+func handler(req events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	switch req.HTTPMethod {
+	case "GET":
+		return handlers.GetUser(req, tableName, dynaClient)
+	case "POST":
+		return handlers.CreateUser(req, tableName, dynaClient)
+	case "PUT":
+		return handlers.UpdateUser(req, tableName, dynaClient)
+	case "DELETE":
+		return handlers.DeleteUser(req, tableName, dynaClient)
+	default:
+		return handlers.UnhandledMethod()
+	}
 }
